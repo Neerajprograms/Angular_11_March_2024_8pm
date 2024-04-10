9e2afc1bf318b06a226a16b951ce5b11aa20c280
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-04-10 21:27:25 +0530

    promise

---
+++ day_25 april_10/ReadFile_Demo.js
@@ -0,0 +1,23 @@
+// Read data from file-1 and file-2 write both the content into file-3
+
+const fs = require('fs');
+
+fs.readFile('./file1.txt', 'utf-8', (err1, data1) => {
+    if (err1) {
+        throw err1;
+    }
+
+    fs.readFile('./file2.txt', 'utf-8', (err2, data2) => {
+        if (err2) {
+            throw err2;
+        }
+        fs.writeFile('./file3.txt', `${data1} ${data2}`, (err3, data3) => {
+            if (err1) {
+                throw err1;
+            }
+            console.log('data written succesfully')
+        })
+
+    })
+
+})
